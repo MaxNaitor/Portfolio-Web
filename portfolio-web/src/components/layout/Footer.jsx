e2d289e6d7da089a136e1a919ae99c47a4297d62
Tiziano Massa <tiziano.max@gmail.com>
2021-04-20 11:30:04 +0200

    modifiche al css

--- portfolio-web/src/components/layout/Footer.jsx
+++ portfolio-web/src/components/layout/Footer.jsx
@@ -5,14 +5,14 @@ import linkedin from '../../img/linkedin.png'
 const Footer = () => {
     return (
         <div className="footer">
-            <div className="row">
-                <div className="col-sm-4">
+            <div className="row no-gutters" style={{height:"100%", width:"100%"}}>
+                <div className="col-sm-4" style={{height:"100%", width:"100%"}}>
                     <a href="https://github.com/MaxNaitor" target="_blank"><img src={github} alt="" /></a>
                 </div>
-                <div className="col-sm-4">
+                <div className="col-sm-4" style={{height:"100%", width:"100%"}}>
                     <a href="https://www.linkedin.com/in/tiziano-massa-061151158/" target="_blank"> <img src={linkedin} alt="" /> </a>
                 </div>
-                <div className="col-sm-4">
+                <div className="col-sm-4" style={{height:"100%", width:"100%"}}>
                     <a href="https://www.instagram.com/ti_pics/" target="_blank"><img src={instagram} alt="" /> </a>
                 </div>
             </div>

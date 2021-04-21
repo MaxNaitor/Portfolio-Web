e5a68db23503000b1b1db7d8fb04d94f41295df4
Tiziano Massa <tiziano.max@gmail.com>
2021-04-21 21:09:49 +0200

    aggiunta visualizzazione cv

--- portfolio-web/src/components/contenuti/CV.jsx
+++ portfolio-web/src/components/contenuti/CV.jsx
@@ -1,11 +1,16 @@
 import { Link } from "react-router-dom";
+import { Document, Page, pdfjs } from "react-pdf";
+pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 const CV = () => {
+
     return (
         <div className="cv">
+            <Document file="/files/Tiziano Massa CV.pdf" className="pdf">
+                <Page pageNumber={1}></Page>
+                <Page pageNumber={2}></Page>
+            </Document>
             <Link to="/files/Tiziano Massa CV.pdf" target="_blank" download> Scarica il CV</Link>
-
-
         </div>
 
     )

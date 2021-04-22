041daa049f0c49ae47ca6eab81229f5dccf57730
Tiziano Massa <tiziano.max@gmail.com>
2021-04-22 14:05:46 +0200

    completate sezioni progetti e tecnologie

---
+++ portfolio-web/src/components/contenuti/Elemento.jsx
@@ -0,0 +1,53 @@
+import github from '../../img/github.png'
+
+
+const Elemento = ({ elemento, tipo }) => {
+
+    const renderComponente = () => {
+        if (tipo === 'tecnologie') {
+            return renderTecnologie()
+        } else if (tipo === 'progetti') {
+            return renderProgetti()
+        }
+    }
+
+    const renderTecnologie = () => {
+        return (
+            <div className="elemento-tech">
+                <h1>{elemento.nome}</h1>
+                <div className="row no-gutters">
+                    <div className="col-md-3">
+                        <img src={elemento.immagine} alt="" />
+                    </div>
+                    <div className="col-md-9">
+                        <h3>{elemento.commento}</h3>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+    const renderProgetti = () => {
+        return (
+            <div className="elemento-tech">
+                <h1>{elemento.nome}</h1>
+                <div className="row no-gutters">
+                    <div className="col-md-3">
+                        <a href={elemento.github} target="_blank"><img src={github} alt="" /></a>
+                    </div>
+                    <div className="col-md-9">
+                        <h3>{elemento.descrizione}</h3>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+    return (
+        <>
+            {renderComponente()}
+        </>
+    )
+}
+
+export default Elemento;
--- portfolio-web/src/components/contenuti/Liste.jsx
+++ portfolio-web/src/components/contenuti/Liste.jsx
@@ -1,19 +1,20 @@
 import { firestore } from "../../App";
 import { useCollectionData } from 'react-firebase-hooks/firestore';
+import Elemento from "./Elemento";
 
 
 
-const Liste = ({tipo}) => {
+const Liste = ({ tipo }) => {
 
     const db = firestore.collection(tipo); //richiamo la raccolta delle tecnologie
     const query = db.orderBy('ordine'); //creo una query ordinata in base all'ordine di studio
     const [lista] = useCollectionData(query);
 
     return (
-        <div>
+        <div className="lista">
             {lista && lista.map(elem =>
-                <h1>{elem.nome}</h1>
-                )}
+                <Elemento elemento={elem} tipo={tipo} key={elem.id} />
+            )}
         </div>
     )
 

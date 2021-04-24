be94a72e6318ce6cad4ce55ea699b6194dfdbb1d
Tiziano Massa <tiziano.max@gmail.com>
2021-04-25 00:21:18 +0200

    importare foto 400 x 600

--- portfolio-web/src/components/photography/exportRitratti.js
+++ portfolio-web/src/components/photography/exportRitratti.js
@@ -7,75 +7,13 @@
 //     caption: "Boats (Jeshu John - designerspics.com)"
 // },
 
-import uno from '../../img/ritratti/1.jpg'
-import due from '../../img/ritratti/2.jpg'
-import tre from '../../img/ritratti/3.jpg'
-import quattro from '../../img/ritratti/4.jpg'
-import cinque from '../../img/ritratti/5.jpg'
-import otto from '../../img/ritratti/8.jpg'
-import nove from '../../img/ritratti/9.jpg'
-import dieci from '../../img/ritratti/10.jpg'
-import undici from '../../img/ritratti/11.jpg'
-import quattordici from '../../img/ritratti/14.jpg'
+import marti from '../../img/ritratti/marti.jpg'
 
 export const RITRATTI = [
+    //esporta le immagini da lightroom 400 x 600
 {
-    src: cinque,
-    thumbnail: cinque,
-    thumbnailWidth: 3,
-    thumbnailHeight: 2
-},
-{
-    src: due,
-    thumbnail: due,
-    thumbnailWidth: 2,
-    thumbnailHeight: 3
-},
-{
-    src: quattro,
-    thumbnail: quattro,
-    thumbnailWidth: 2,
-    thumbnailHeight: 3
-},
-{
-    src: otto,
-    thumbnail: otto,
-    thumbnailWidth: 2,
-    thumbnailHeight: 3
-},
-{
-    src: nove,
-    thumbnail: nove,
-    thumbnailWidth: 2,
-    thumbnailHeight: 3
-},
-{
-    src: uno,
-    thumbnail: uno,
-    thumbnailWidth: 2,
-    thumbnailHeight: 3
-},
-{
-    src: tre,
-    thumbnail: tre,
-    thumbnailWidth: 2,
-    thumbnailHeight: 3
-},
-{
-    src: dieci,
-    thumbnail: dieci,
-    thumbnailWidth: 2,
-    thumbnailHeight: 3
-},
-{
-    src: undici,
-    thumbnail: undici,
-    thumbnailWidth: 2,
-    thumbnailHeight: 3
-},
-{
-    src: quattordici,
-    thumbnail: quattordici,
+    src: marti,
+    thumbnail: marti,
     thumbnailWidth: 2,
     thumbnailHeight: 3
 },

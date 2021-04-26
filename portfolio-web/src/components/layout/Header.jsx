356d590d1799e4c6bef5b7ec68d215a815cce0b0
Tiziano Massa <tiziano.max@gmail.com>
2021-04-26 18:56:04 +0200

    prova again again

--- portfolio-web/src/components/layout/Header.jsx
+++ portfolio-web/src/components/layout/Header.jsx
@@ -7,14 +7,14 @@ const Header = () => {
     return (
         <div className="header">
             <div className="row">
-                <div className="col-md-3">
-                    <Link to="/development"> <h5>Web Development</h5> </Link>
+                <div className="col-md-3" onClick={() => history.push("/development")}>
+                    <h5>Web Development</h5>
                 </div>
                 <div className="col-md-6" onClick={() => history.push("/homepage")}>
                     <h1>Tiziano Massa</h1>
                 </div>
-                <div className="col-md-3">
-                    <Link to="/photography">Fotografia</Link>
+                <div className="col-md-3" onClick={() => history.push("/photography")}>
+                    <h5>Fotografia</h5>
                 </div>
 
             </div>
